171004fbbc9542fcce492596fbb7f3a19afdf1c1
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-02-10 12:45:08 +0400

    Update coroutine.js

--- coroutine.js
+++ coroutine.js
@@ -2,7 +2,7 @@ function myCoroutine(gen) {
   let iter = gen();
   return new Promise((res) => {
     function isPromis(next) {
-      if (next.done == true) res();
+      if (next.done) res();
       else if (next.value.then instanceof Function)
         next.value.then((x) => isPromis(iter.next(x)));
       else isPromis(iter.next(next.value));
