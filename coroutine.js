9774251e857a645bd196fd9cddddcdae55f2a27e
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-02-10 12:20:23 +0400

    Create coroutine.js

---
+++ coroutine.js
@@ -0,0 +1,12 @@
+function coroutine1(gen) {
+  let iter = gen();
+  return new Promise((res) => {
+    function isPromis(next) {
+      if (next.done == true) res();
+      else if (next.value.then instanceof Function)
+        next.value.then((x) => isPromis(iter.next(x)));
+      else isPromis(iter.next(next.value));
+    }
+    isPromis(iter.next());
+  });
+}
