193ab035cb0ad1becac60c0ba4e01eadf0f2d81e
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-03-11 21:10:36 +0400

    Update postsPhotos.js

--- PicsApi/postRout/postsPhotos.js
+++ PicsApi/postRout/postsPhotos.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const Post = require("../model/Post");
-const verifyToken = require("../tokensWork/verifyToken");
 
 const router = express.Router();
 
 // /api/post/photos/:id
-router.get("/:id", verifyToken, async (req, res) => {
+router.get("/:id", async (req, res) => {
   const post = await Post.findOne({ _id: req.params.id });
   if (!post) return res.status(400).send("Cant finde post with such id");
   const { images } = post;
