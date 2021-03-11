8af440077eeb511e523174e85c727687d966d2fa
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-03-11 21:08:56 +0400

    Update lastPosts.js

--- PicsApi/postRout/lastPosts.js
+++ PicsApi/postRout/lastPosts.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const Post = require("../model/Post");
-const verifyToken = require("../tokensWork/verifyToken");
 
 const router = express.Router();
 
 // /api/posts/get/last
-router.get("/last", verifyToken, async (req, res) => {
+router.get("/last",  async (req, res) => {
   res.send(await Post.find().sort({ createdAt: -1 }).limit(10))
 });
 
