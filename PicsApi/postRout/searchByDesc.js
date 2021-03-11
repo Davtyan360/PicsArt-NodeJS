93c0c6b003dffb3a301dc76d373a4ed5221f4825
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-03-11 21:10:11 +0400

    Update searchByDesc.js

--- PicsApi/postRout/searchByDesc.js
+++ PicsApi/postRout/searchByDesc.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const querystring = require("querystring");
 
-const verifyToken = require("../tokensWork/verifyToken");
 const Post = require("../model/Post");
 
 const router = express.Router();
 
 //  /api/posts/desc/:desc
-router.get("/:desc", verifyToken, async (req, res) => {
+router.get("/:desc",  async (req, res) => {
   let q = querystring.parse(req.params.desc);
   let post = await Post.find();
   if (q.userMail) post = post.filter((val) => val.userMail == q.userMail);
