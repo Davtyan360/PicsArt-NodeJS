96fb5467f3492196398129ab1cb3b39ad03ba44d
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-03-20 11:13:47 +0400

    Update EventEmitterImplement.js

--- EventEmitterImplement.js
+++ EventEmitterImplement.js
@@ -1,10 +1,14 @@
 class EventEmit {
   static obj = {};
   static on(path, func) {
-    this.obj[path] = { path: path, func: func };
+    if (typeof func === "function" && typeof path === "string")
+      this.obj[path] = { path: path, func: func };
   }
   static emit(path, ...args) {
-    if (!this.obj[path]) return false;
+
+    if (!this.obj[path]) 
+      return false;
+
     this.obj[path].func(...args);
     return true;
   }
